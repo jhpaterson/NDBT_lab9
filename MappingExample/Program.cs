3b09fb7361268015a0d7767c38e602501057f1c6
Jim Paterson <James.Paterson@gcu.ac.uk>
2013-03-17 21:13:26 +0000

    initial commit

---
+++ MappingExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using MongoDB;
+
+namespace MappingExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
